a632243cb05e4366be492782587ce537617c8fdd
Eliel <70238970+griffan113@users.noreply.github.com>
2021-09-13 10:45:14 -0300

    chore: add styling to Input and Buttom

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
+import Input from '../../components/Input';
 import logoImage from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+import Buttom from '../../components/Buttom';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImage} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Buttom onPress={() => {}}>Entrar</Buttom>
     </Container>
   );
 };
