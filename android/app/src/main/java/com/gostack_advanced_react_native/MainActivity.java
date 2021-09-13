ddfa52df193149454fa1579dd0485becc3048bf7
Eliel <70238970+griffan113@users.noreply.github.com>
2021-09-13 09:28:43 -0300

    chore: add React Navigation - set up routes

--- android/app/src/main/java/com/gostack_advanced_react_native/MainActivity.java
+++ android/app/src/main/java/com/gostack_advanced_react_native/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gostack_advanced_react_native;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "gostack_advanced_react_native";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
