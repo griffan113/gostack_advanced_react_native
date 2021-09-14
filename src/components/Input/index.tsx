5e6542abde55e4fddc3e58d8f040d8a7f27efeb9
Eliel <70238970+griffan113@users.noreply.github.com>
2021-09-14 10:00:54 -0300

    chore: add border styles to Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,11 @@
 import { useField } from '@unform/core';
 import React, {
   forwardRef,
+  useCallback,
   useEffect,
   useImperativeHandle,
   useRef,
+  useState,
 } from 'react';
 import { TextInputProps } from 'react-native';
 
@@ -26,6 +28,17 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   { name, icon, ...props },
   ref,
 ) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
   const inputElementRef = useRef<any>(null);
 
   const { registerField, error, defaultValue = '', fieldName } = useField(name);
@@ -55,8 +68,12 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }));
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
 
       <TextInput
         ref={inputElementRef}
@@ -65,6 +82,8 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         defaultValue={defaultValue}
         {...props}
       ></TextInput>
